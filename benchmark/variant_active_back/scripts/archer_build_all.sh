bf415412f2fb7c53eb94263ed584e4834c3710ee
Christodoulos Stylianou <s1887443@ed.ac.uk>
2020-08-13 22:54:05 +0300

    updated benchmark scripts

--- benchmark/variant_active_back/scripts/archer_build_all.sh
+++ benchmark/variant_active_back/scripts/archer_build_all.sh
@@ -3,9 +3,9 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 MORPHEUS_PATH="$SCRIPT_PATH/../../.."
 
-$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer gcc 7.3.0"
-$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer gcc 8.3.0"
-$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer gcc 9.3.0"
-$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer gcc 10.1.0"
+$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer" "gcc" "7.3.0"
+$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer" "gcc" "8.3.0"
+$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer" "gcc" "9.3.0"
+$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer" "gcc" "10.1.0"
 
-$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer intel 19.5"
+$MORPHEUS_PATH/benchmark/variant_active_back/scripts/build.sh "archer" "intel" "19.5"
--- benchmark/variant_active_front/scripts/cirrus_build_all.sh
+++ benchmark/variant_active_front/scripts/cirrus_build_all.sh
@@ -3,10 +3,10 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 MORPHEUS_PATH="$SCRIPT_PATH/../../.."
 
-$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus gcc 8.2.0"
-$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus gcc 8.3.0"
-$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus gcc 9.3.0"
-$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus gcc 10.1.0"
+$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus" "gcc" "8.2.0"
+$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus" "gcc" "8.3.0"
+$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus" "gcc" "9.3.0"
+$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus" "gcc" "10.1.0"
 
-$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus intel 19.5"
-$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus intel 20.1"
+$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus" "intel" "19.5"
+$MORPHEUS_PATH/benchmark/variant_active_front/scripts/build.sh "cirrus" "intel" "20.1"
