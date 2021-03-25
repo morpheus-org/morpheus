e5332063ef82e5d7251c9eca4e3c284898a4a223
cstyl <cstyl16@gmail.com>
2021-03-26 00:36:57 +0200

    added initialization of morpheus and kokkos runtime

---
+++ morpheus/core/core.hpp
@@ -0,0 +1,16 @@
+#ifndef MORPHEUS_CORE_CORE_HPP
+#define MORPHEUS_CORE_CORE_HPP
+
+namespace Morpheus {
+
+struct InitArguments;
+
+void initialize(int& argc, char* argv[]);
+void initialize(InitArguments args);
+void print_configuration(std::ostream& out, const bool detail);
+void finalize();
+}  // namespace Morpheus
+
+#include <morpheus/core/impl/core.inl>
+
+#endif  // MORPHEUS_CORE_CORE_HPP
