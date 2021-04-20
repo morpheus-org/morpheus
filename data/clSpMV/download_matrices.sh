d06a67024640b4cd7c9194c93b9ff27fc0b8931a
cstyl <cstyl16@gmail.com>
2021-04-20 11:23:46 +0100

    updated benchmark scripts

--- data/clSpMV/download_matrices.sh
+++ data/clSpMV/download_matrices.sh
@@ -5,14 +5,14 @@ MATRICES=("https://suitesparse-collection-website.herokuapp.com/MM/Williams/pdb1
           "https://suitesparse-collection-website.herokuapp.com/MM/Williams/cant.tar.gz"
           "https://suitesparse-collection-website.herokuapp.com/MM/Boeing/pwtk.tar.gz"
           "https://suitesparse-collection-website.herokuapp.com/MM/Bova/rma10.tar.gz"
-          "https://suitesparse-collection-website.herokuapp.com/MM/QCD/conf5_4-8x8-05.tar.gz"
+          # "https://suitesparse-collection-website.herokuapp.com/MM/QCD/conf5_4-8x8-05.tar.gz"
           "https://suitesparse-collection-website.herokuapp.com/MM/DNVS/shipsec1.tar.gz"
           "https://suitesparse-collection-website.herokuapp.com/MM/Williams/mac_econ_fwd500.tar.gz"
           "https://suitesparse-collection-website.herokuapp.com/MM/Williams/mc2depi.tar.gz"
           "https://suitesparse-collection-website.herokuapp.com/MM/Williams/cop20k_A.tar.gz"
           "https://suitesparse-collection-website.herokuapp.com/MM/Hamm/scircuit.tar.gz"
-          "https://suitesparse-collection-website.herokuapp.com/MM/Williams/webbase-1M.tar.gz"
-          "https://suitesparse-collection-website.herokuapp.com/MM/Mittelmann/rail4284.tar.gz"
+          # "https://suitesparse-collection-website.herokuapp.com/MM/Williams/webbase-1M.tar.gz"
+          # "https://suitesparse-collection-website.herokuapp.com/MM/Mittelmann/rail4284.tar.gz"
           )
 
 for matrix in "${MATRICES[@]}"
