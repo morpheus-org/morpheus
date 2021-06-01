693c2ba1836554c319443521f52c976b590c1871
cstyl <cstyl16@gmail.com>
2021-06-01 14:35:02 +0100

    created one high level morpheus header

---
+++ morpheus/containers/containers.hpp
@@ -0,0 +1,34 @@
+/**
+ * containers.hpp
+ *
+ * EPCC, The University of Edinburgh
+ *
+ * (c) 2021 The University of Edinburgh
+ *
+ * Contributing Authors:
+ * Christodoulos Stylianou (c.stylianou@ed.ac.uk)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MORPHEUS_CONTAINERS_CONTAINERS_HPP
+#define MORPHEUS_CONTAINERS_CONTAINERS_HPP
+
+#include <morpheus/containers/coo_matrix.hpp>
+#include <morpheus/containers/csr_matrix.hpp>
+#include <morpheus/containers/dense_matrix.hpp>
+#include <morpheus/containers/dia_matrix.hpp>
+#include <morpheus/containers/dynamic_matrix.hpp>
+#include <morpheus/containers/vector.hpp>
+
+#endif  // MORPHEUS_CONTAINERS_CONTAINERS_HPP
