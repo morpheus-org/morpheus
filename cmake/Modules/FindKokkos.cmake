a03bc1b51ad95ece8eb732126ece533d1a6dbb1a
cstyl <cstyl16@gmail.com>
2021-07-14 19:54:51 +0300

    removed unecessary files

--- cmake/Modules/FindKokkos.cmake
+++
@@ -1,9 +0,0 @@
-IF (NOT Kokkos_ROOT)
-  SET(Kokkos_ROOT $ENV{METIS_ROOT})
-ENDIF()
-#we need to find one of the valid versions from the list below
-KOKKOSKERNELS_FIND_IMPORTED(METIS
-  LIBRARY metis
-  LIBRARY_PATHS ${METIS_LIBRARY_DIRS}
-  HEADERS metis.h
-  HEADER_PATHS ${METIS_INCLUDE_DIRS})
