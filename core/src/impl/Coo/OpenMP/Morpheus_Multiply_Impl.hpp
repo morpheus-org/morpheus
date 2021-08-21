8ec4e55e83338997c9fd4e3b967b2974f375451e
cstyl <cstyl16@gmail.com>
2021-08-21 21:51:06 +0100

    partial implementation of Coo spmv with openmp

--- core/src/impl/Coo/OpenMP/Morpheus_Multiply_Impl.hpp
+++ core/src/impl/Coo/OpenMP/Morpheus_Multiply_Impl.hpp
@@ -30,9 +30,49 @@
 #include <Morpheus_TypeTraits.hpp>
 #include <Morpheus_FormatTags.hpp>
 #include <Morpheus_AlgorithmTags.hpp>
+#include <Morpheus_Scan.hpp>
+
+#include <limits>
 
 namespace Morpheus {
 namespace Impl {
+
+template <typename T>
+T _split_work(const T load, const T workers, const T worker_id) {
+  const T unifload = load / workers;  // uniform distribution
+  const T rem      = load - unifload * workers;
+  T bound;
+
+  //  round-robin assignment of the remaining work
+  if (worker_id <= rem) {
+    bound = (unifload + 1) * worker_id;
+  } else {
+    bound = (unifload + 1) * rem + unifload * (worker_id - rem);
+  }
+
+  return bound;
+}
+
+int threads() {
+  int t = 1;
+#pragma omp parallel
+  { t = omp_get_num_threads(); }
+
+  return t;
+}
+
+template <typename T>
+int is_row_stop(T container, typename T::index_type idx,
+                typename T::index_type len = 0) {
+  return container[idx] != container[idx + len + 1];
+}
+
+template <typename T>
+int is_first_row_stop_in_workgroup(T container,
+                                   typename T::index_type start_idx,
+                                   typename T::index_type cur_index) {
+  return container[start_idx] != container[cur_index];
+}
 
 template <typename ExecSpace, typename LinearOperator, typename MatrixOrVector1,
           typename MatrixOrVector2>
@@ -45,12 +85,65 @@ inline void multiply(
         Morpheus::has_access_v<typename ExecSpace::execution_space,
                                LinearOperator, MatrixOrVector1,
                                MatrixOrVector2>>* = nullptr) {
+  using ValueType = typename LinearOperator::value_type;
   using IndexType = typename LinearOperator::index_type;
+  using KeyType   = typename LinearOperator::index_type;
+  using vector    = MatrixOrVector1;
 
-// assumes A is sorted
-#pragma omp parallel for
-  for (IndexType n = 0; n < A.nnnz(); n++) {
-    y[A.row_indices[n]] += A.values[n] * x[A.column_indices[n]];
+  const IndexType nthreads = threads();
+  vector out("out", A.nnnz(), 0), last_partial_sums(nthreads, 0);
+  // Initialize to special value
+  vector grp_sum(nthreads,
+                 std::numeric_limits<typename vector::value_type>::max());
+
+#pragma omp parallel
+  {
+    const IndexType tid = omp_get_thread_num();
+
+    // evenly divide workload based on nnz elements
+    // each workgroup is assigned to one thread
+    const IndexType chunk = A.nnnz() / nthreads;
+    const IndexType start = _split_work(A.nnnz(), nthreads, tid);
+    const IndexType stop  = _split_work(A.nnnz(), nthreads, tid + 1);
+    const IndexType last  = stop - 1;
+
+    // multiply value arrays with the corresponding vector values
+    // indexed by the colum_indices array;
+    for (IndexType n = start; n < stop; n++) {
+      out[n] = A.values[n] * x[A.column_indices[n]];
+    }
+
+    // perform a segmented scan by key using the row_indices array as key
+    Morpheus::inclusive_scan_by_key<Kokkos::Serial>(A.row_indices, out, out,
+                                                    stop - start, start);
+
+    // Check if last value is at a row stop and write partial_sum_out
+    if (tid == nthreads - 1) {
+      // Always row stop at the end of array
+      last_partial_sums[tid] = ValueType(0);
+    } else {
+      if (is_row_stop(A.row_indices, last))
+        last_partial_sums[tid] = ValueType(0);  // Zero at row stop
+      else
+        last_partial_sums[tid] = out[last];
+    }
+
+#pragma omp barrier  // partial_sum_out and out must be available to all
+
+    ValueType partial_sum =
+        (tid == 0) ? ValueType(0) : last_partial_sums[tid - 1];
+
+    for (IndexType n = start; n < last; n++) {
+      if(is_row_stop(A.row_indices,n){
+        if (is_first_row_stop_in_workgroup(A.row_indices, start, n)) {
+          // TODO: A row might span multiple threads, need to get the partial
+          // sum from those too.
+          y[A.row_indices[n]] = out[n] + partial_sum;
+        } else {
+          y[A.row_indices[n]] = out[n];
+        }
+      }
+    }
   }
 }
 
