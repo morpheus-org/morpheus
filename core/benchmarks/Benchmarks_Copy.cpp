54b12416d677f5ee3bfd7c8804da15adfcdaea0f
cstyl <cstyl16@gmail.com>
2021-09-23 18:55:32 +0300

    adding copy benchmark

--- core/benchmarks/Benchmarks_Copy.cpp
+++ core/benchmarks/Benchmarks_Copy.cpp
@@ -23,19 +23,110 @@
 
 #include <Morpheus_Core.hpp>
 
-// TODO:
-// - Load data on Coo.
-// - COO: COO_DEEP, COO_ELEM.
-// - CSR: CSR_DEEP, CSR_ELEM.
-// - DIA: DIA_DEEP, DIA_ELEM.
-// - DYNCOO: COO_DEEP, COO_ELEM.
-// - DYNCSR: CSR_DEEP, CSR_ELEM.
-// - DYNDIA: DIA_DEEP, DIA_ELEM.
-// - Bench copy.
+#include "timer.hpp"
+#include <cstdlib>
+
+using ExecSpace = Kokkos::Serial;
+
+using coo = Morpheus::CooMatrix<double, int, ExecSpace>;
+using csr = Morpheus::CsrMatrix<double, int, ExecSpace>;
+using dia = Morpheus::DiaMatrix<double, int, ExecSpace>;
+using dyn = Morpheus::DynamicMatrix<double, int, ExecSpace>;
+
+auto reps = 50;
+Morpheus::TimerPool timer;
+
+template <typename Fmt>
+void deep(const Fmt& A, enum Morpheus::TimerPool::timer_id tidx,
+          const std::string& fn_str) {
+  for (auto rep = 0; rep < reps; rep++) {
+    timer.start(tidx);
+    auto B = Morpheus::create_mirror<ExecSpace>(A);
+    Morpheus::copy(A, B);
+    timer.stop(tidx);
+
+    std::cout << "\tStep " << rep + 1 << "/" << reps << "\t" << fn_str
+              << std::endl;
+  }
+}
+
+template <typename Fmt>
+void elem(const Fmt& A, enum Morpheus::TimerPool::timer_id tidx,
+          const std::string& fn_str) {
+  for (auto rep = 0; rep < reps; rep++) {
+    timer.start(tidx);
+    Fmt B;
+    Morpheus::convert(A, B);
+    timer.stop(tidx);
+
+    std::cout << "\tStep " << rep + 1 << "/" << reps << "\t" << fn_str
+              << std::endl;
+  }
+}
 
 int main(int argc, char* argv[]) {
   Morpheus::initialize(argc, argv);
-  {}
+  {
+    {
+      if (argc != 3) {
+        std::cout
+            << "Benchmarks_Convert.cpp requires 2 input arguments to be given "
+               "at runtime (filename, reps). Only received "
+            << argc - 1 << "!" << std::endl;
+        exit(-1);
+      }
+
+      std::string filename = argv[1];
+      reps                 = atoi(argv[2]);
+
+      std::cout << "\nRunning Benchmarks_Convert.cpp with:\n";
+      std::cout << "\tFilename:\t" << filename << "\n";
+      std::cout << "\tReps:    \t" << reps << "\n\n";
+
+      coo Aio;
+
+      try {
+        timer.start(timer.IO_READ);
+        Morpheus::Io::read_matrix_market_file(Aio, filename);
+        timer.stop(timer.IO_READ);
+      } catch (Morpheus::NotImplementedException& e) {
+        std::cerr << "Exception Raised:: " << e.what() << std::endl;
+        exit(0);
+      }
+
+      std::cout << "Starting experiment:" << std::endl;
+
+      {
+        coo A;
+        Morpheus::convert(Aio, A);
+
+        deep<coo>(A, timer.COPY_COO_DEEP, "deep<coo>");
+        elem<coo>(A, timer.COPY_COO_ELEM, "elem<coo>");
+      }
+
+      {
+        csr A;
+        Morpheus::convert(Aio, A);
+
+        deep<csr>(A, timer.COPY_CSR_DEEP, "deep<csr>");
+        elem<csr>(A, timer.COPY_CSR_ELEM, "elem<csr>");
+      }
+
+      {
+        try {
+          dia A;
+          Morpheus::convert(Aio, A);
+
+          deep<dia>(A, timer.COPY_DIA_DEEP, "deep<dia>");
+          elem<dia>(A, timer.COPY_DIA_ELEM, "elem<dia>");
+        } catch (std::exception& e) {
+          std::cerr << "Exception Raised:: " << e.what() << std::endl;
+        }
+      }
+    }
+  }
+
+  timer.statistics();
   Morpheus::finalize();
 
   return 0;
