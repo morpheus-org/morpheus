cba8b63daee26f9039bf77f44df5eb3203fe8f78
cstyl <cstyl16@gmail.com>
2021-11-09 19:31:13 +0000

    fixing SPMV OpenMP version for COO format

--- core/src/impl/Coo/OpenMP/Morpheus_Multiply_Impl.hpp
+++ core/src/impl/Coo/OpenMP/Morpheus_Multiply_Impl.hpp
@@ -53,98 +53,59 @@ inline void multiply(
         Morpheus::is_OpenMP_space_v<ExecSpace> &&
         Morpheus::has_access_v<typename ExecSpace::execution_space, Matrix,
                                Vector>>* = nullptr) {
-  using ValueType = typename Matrix::value_type;
-  using IndexType = typename Matrix::index_type;
-  using vector    = Vector;
+  using value_type = typename Matrix::value_type;
+  using index_type = typename Matrix::index_type;
+  const index_type max_threads =
+      A.nnnz() < threads<index_type>() ? A.nnnz() : threads<index_type>();
 
-  if (A.nnnz() < threads<IndexType>()) {
-    omp_set_num_threads(A.nnnz());
+#pragma omp parallel for
+  for (index_type n = 0; n < A.nrows(); n++) {
+    y[n] = value_type(0);
   }
-  const IndexType nthreads = threads<IndexType>();
-  const ValueType max_val  = std::numeric_limits<ValueType>::max();
-  vector out("out", A.nnnz(), 0);
-  // Initialize to special value
-  vector grp_sum(nthreads, max_val);
 
-#pragma omp parallel
+#pragma omp parallel num_threads(max_threads)
   {
-    const IndexType tid = omp_get_thread_num();
+    const index_type nthreads = omp_get_num_threads();
+    const index_type tid      = omp_get_thread_num();
 
-    // evenly divide workload based on nnz elements
-    // each workgroup is assigned to one thread
-    const IndexType start = _split_work(A.nnnz(), nthreads, tid);
-    const IndexType stop  = _split_work(A.nnnz(), nthreads, tid + 1);
-    const IndexType last  = stop - 1;
+    const index_type thread_start = _split_work(A.nnnz(), nthreads, tid);
+    const index_type thread_stop  = _split_work(A.nnnz(), nthreads, tid + 1);
 
-    // multiply value arrays with the corresponding vector values
-    // indexed by the colum_indices array;
-    for (IndexType n = start; n < stop; n++) {
-      out[n] = A.cvalues(n) * x[A.ccolumn_indices(n)];
-    }
+    index_type first_row_start = thread_stop, last_row_stop = thread_stop;
 
-    // perform a segmented scan by key using the row_indices array as key
-    Morpheus::inclusive_scan_by_key<Kokkos::Serial>(A.crow_indices(), out, out,
-                                                    stop - start, start);
-
-    const IndexType has_row_stop =
-        (tid == nthreads - 1)
-            ? 1
-            : is_row_stop(A.crow_indices(), start, last) ||
-                  is_row_stop(A.crow_indices(), start, start + 1);
-    const IndexType spans_workgroups =
-        (tid == 0) ? ValueType(0)
-                   : !is_row_stop(A.crow_indices(), start - 1, start);
-
-    // Adjacent synchronization between workgroups:
-    // Workgroup 0 updates the first entry ‘grp_sum[0]’ with its last partial
-    // sum. For a subsequent workgroup with id X, if it does not contain a row
-    // stop, it waits for the entry ‘grp_sum[X-1]’ to be changed from the
-    // initial value, i.e., updated by workgroup (X-1), and then updates
-    // ‘grp_sum[X]’ with the sum of its last partial sum and ‘grp_sum[X-1]’. If
-    // a workgroup contains a row stop, it breaks such chained updates and
-    // directly updates ‘grp_sum[X]’ with its last partial sum.
-    if (spans_workgroups && !has_row_stop && tid != 0) {
-      // wait for the previous group to update its sum
-      while (grp_sum[tid - 1] == max_val) {
-        // Make sure the read picks up a good copy from memory
-#pragma omp flush(grp_sum)
-      };
-      grp_sum[tid] = out[last] + grp_sum[tid - 1];
-      // Make sure other threads can see my write.
-#pragma omp flush(grp_sum)
-    } else {
-      grp_sum[tid] = out[last];
-      // Make sure other threads can see my write.
-#pragma omp flush(grp_sum)
-    }
-
-    IndexType is_first_stop = 1;
-
-    for (IndexType n = start; n < stop; n++) {
-      IndexType is_stop = (tid == nthreads - 1 && n == last)
-                              ? 1
-                              : is_row_stop(A.crow_indices(), n, n + 1);
-      if (is_stop) {
-        ValueType R;
-        if (is_first_stop) {  // in workgroup
-          is_first_stop = 0;
-          if (spans_workgroups) {
-            // wait for the previous group to update its sum
-            while (grp_sum[tid - 1] == max_val) {
-              // Make sure the read picks up a good copy from memory
-#pragma omp flush(grp_sum)
-            };
-            R = out[n] + grp_sum[tid - 1];
-          } else {
-            R = out[n];
-          }
-        } else {
-          R = out[n];
-        }
-
-        y[A.crow_indices(n)] = R;
+    for (index_type n = thread_start; n < thread_stop - 1; n++) {
+      if (A.crow_indices(n) != A.crow_indices(n + 1)) {
+        first_row_start = n;
+        break;
       }
     }
+
+    for (index_type n = thread_stop; n < thread_start + 1; n--) {
+      if (A.crow_indices(n) != A.crow_indices(n - 1)) {
+        last_row_stop = n;
+        break;
+      }
+    }
+
+    value_type temp = value_type(0);
+    for (index_type n = thread_start; n < first_row_start; n++) {
+      temp += A.cvalues(n) * x(A.ccolumn_indices(n));
+    }
+
+#pragma omp atomic
+    y[A.crow_indices(thread_start)] += temp;
+
+    for (index_type n = first_row_start; n < last_row_stop; n++) {
+      y[A.crow_indices(n)] += A.cvalues(n) * x(A.ccolumn_indices(n));
+    }
+
+    temp = value_type(0);
+    for (index_type n = last_row_stop; n < thread_stop; n++) {
+      temp += A.cvalues(n) * x(A.ccolumn_indices(n));
+    }
+
+#pragma omp atomic
+    y[A.crow_indices(thread_stop)] += temp;
   }
 }
 
