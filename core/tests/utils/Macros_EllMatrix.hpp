4090eca1695e9b8410668db98f889272cc271229
cstyl <cstyl16@gmail.com>
2022-12-29 13:23:52 +0000

    Removed missed type definition in Macros_EllMatrix

--- core/tests/utils/Macros_EllMatrix.hpp
+++ core/tests/utils/Macros_EllMatrix.hpp
@@ -36,8 +36,6 @@
 #define CHECK_ELL_SIZES(A, num_rows, num_cols, num_nnz, num_entries_per_row, \
                         align)                                               \
   {                                                                          \
-    using container_size_type =                                              \
-        typename std::remove_reference<decltype(A.nrows())>::type;           \
     EXPECT_EQ(A.nrows(), num_rows);                                          \
     EXPECT_EQ(A.ncols(), num_cols);                                          \
     EXPECT_EQ(A.nnnz(), num_nnz);                                            \
@@ -45,20 +43,19 @@
     EXPECT_EQ(A.alignment(), align);                                         \
     EXPECT_EQ(A.column_indices().nrows(), num_rows);                         \
     EXPECT_EQ(A.column_indices().view().extent(0), num_rows);                \
-    EXPECT_EQ(A.column_indices().ncols(),                                    \
-              Morpheus::Impl::get_pad_size<container_size_type>(             \
-                  num_entries_per_row, align));                              \
-    EXPECT_EQ(A.column_indices().view().extent(1),                           \
-              Morpheus::Impl::get_pad_size<container_size_type>(             \
-                  num_entries_per_row, align));                              \
+    EXPECT_EQ(                                                               \
+        A.column_indices().ncols(),                                          \
+        Morpheus::Impl::get_pad_size<size_t>(num_entries_per_row, align));   \
+    EXPECT_EQ(                                                               \
+        A.column_indices().view().extent(1),                                 \
+        Morpheus::Impl::get_pad_size<size_t>(num_entries_per_row, align));   \
     EXPECT_EQ(A.values().nrows(), num_rows);                                 \
     EXPECT_EQ(A.values().view().extent(0), num_rows);                        \
-    EXPECT_EQ(A.values().ncols(),                                            \
-              Morpheus::Impl::get_pad_size<container_size_type>(             \
-                  num_entries_per_row, align));                              \
-    EXPECT_EQ(A.values().view().extent(1),                                   \
-              Morpheus::Impl::get_pad_size<container_size_type>(             \
-                  num_entries_per_row, align));                              \
+    EXPECT_EQ(A.values().ncols(), Morpheus::Impl::get_pad_size<size_t>(      \
+                                      num_entries_per_row, align));          \
+    EXPECT_EQ(                                                               \
+        A.values().view().extent(1),                                         \
+        Morpheus::Impl::get_pad_size<size_t>(num_entries_per_row, align));   \
   }
 
 /**
